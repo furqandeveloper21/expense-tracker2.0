837ef32e736d8d34d267cde0f5291a292d40bd2e
furqandeveloper21 <furqanmalik2877@gmail.com>
2021-01-20 01:12:28 -0800

    Components
    Initialize components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import {AccountSummary} from "./components/AccountSummary";
+import {TransactionHistory} from "./components/TransactionHistory";
+import {AddTransaction} from "./components/AddTransaction";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+     <div className="container">
+       <Header />
+       <Balance />
+       <AccountSummary />
+       <TransactionHistory />
+       <AddTransaction />
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Balance () {
+
+    return (
+        <div>
+            <h2>YOUR BALANCE</h2>
+            <p>$260.00</p>
+        </div>
+    );
+
+}
+
+export default Balance;
